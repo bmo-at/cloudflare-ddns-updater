3f355e1c4b94c04f3dd68997e04d9c0099ad4354
bmo-at <jan-robin@aumann.org>
2023-12-02 13:12:38 +0100

    move context out of loop, add newlines to loglines

--- main.go
+++ main.go
@@ -30,26 +30,26 @@ type CloudflareDDNSUpdaterApplication struct {
 }
 
 func (c *CloudflareDDNSUpdaterApplication) configure() {
-	c.logger.Infof("CLOUDFLARE DDNS configuration started " + strings.Repeat("-", 12))
+	c.logger.Infof("CLOUDFLARE DDNS configuration started " + strings.Repeat("-", 12) + "\n")
 
 	if api_token, exists := os.LookupEnv(API_TOKEN_ENV_VARIABLE_NAME); exists {
 		c.api_token = api_token
 	} else {
-		c.logger.Errorf("no API token found in env var '%s'", API_TOKEN_ENV_VARIABLE_NAME)
+		c.logger.Errorf("no API token found in env var '%s'\n", API_TOKEN_ENV_VARIABLE_NAME)
 		c.exit()
 	}
 
 	if zone_name, exists := os.LookupEnv(ZONE_ENV_VARIABLE_NAME); exists {
 		c.zone_name = zone_name
 	} else {
-		c.logger.Errorf("no zone name found in env var '%s'", ZONE_ENV_VARIABLE_NAME)
+		c.logger.Errorf("no zone name found in env var '%s'\n", ZONE_ENV_VARIABLE_NAME)
 		c.exit()
 	}
 
 	if record_name, exists := os.LookupEnv("CLOUDFLARE_RECORD_NAME"); exists {
 		c.record_name = record_name
 	} else {
-		c.logger.Errorf("no record name found in env var '%s'", RECORD_ENV_VARIABLE_NAME)
+		c.logger.Errorf("no record name found in env var '%s'\n", RECORD_ENV_VARIABLE_NAME)
 		c.exit()
 	}
 
@@ -59,15 +59,15 @@ func (c *CloudflareDDNSUpdaterApplication) configure() {
 		c.ip_info_url = "https://ipinfo.io/ip"
 	}
 
-	c.logger.Infof("CLOUDFLARE DDNS configuration finished " + strings.Repeat("-", 11))
+	c.logger.Infof("CLOUDFLARE DDNS configuration finished " + strings.Repeat("-", 11) + "\n")
 }
 
 func (c *CloudflareDDNSUpdaterApplication) initialize() {
-	c.logger.Infof("CLOUDFLARE DDNS initialization started " + strings.Repeat("-", 11))
+	c.logger.Infof("CLOUDFLARE DDNS initialization started " + strings.Repeat("-", 11) + "\n")
 
 	api, err := cloudflare.NewWithAPIToken(c.api_token)
 	if err != nil {
-		c.logger.Errorf("could not create cloudflare api client with the provided token, %s", err.Error())
+		c.logger.Errorf("could not create cloudflare api client with the provided token, %s\n", err.Error())
 		c.exit()
 	}
 	c.api = api
@@ -75,45 +75,45 @@ func (c *CloudflareDDNSUpdaterApplication) initialize() {
 	_, err = http.Get(c.ip_info_url)
 
 	if err != nil {
-		c.logger.Errorf("current ip info endpoint '%s' could not be requested: %s", c.ip_info_url, err.Error())
+		c.logger.Errorf("current ip info endpoint '%s' could not be requested: %s\n", c.ip_info_url, err.Error())
 	}
 
-	c.logger.Infof("CLOUDFLARE DDNS initialization finished " + strings.Repeat("-", 10))
+	c.logger.Infof("CLOUDFLARE DDNS initialization finished " + strings.Repeat("-", 10) + "\n")
 }
 
 func (c *CloudflareDDNSUpdaterApplication) update(ctx context.Context) {
-	c.logger.Infof("CLOUDFLARE DDNS update started " + strings.Repeat("-", 19))
+	c.logger.Infof("CLOUDFLARE DDNS update started " + strings.Repeat("-", 19) + "\n")
 
 	ip_response, err := http.Get(c.ip_info_url)
 
 	if err != nil {
-		c.logger.Errorf("error when requesting the current ip from '%s': %s", c.ip_info_url, err.Error())
+		c.logger.Errorf("error when requesting the current ip from '%s': %s\n", c.ip_info_url, err.Error())
 		c.exit()
 	}
 
 	ip_bytes, err := io.ReadAll(ip_response.Body)
 
 	if err != nil {
-		c.logger.Errorf("error reading the body of the ip request response: %s", err.Error())
+		c.logger.Errorf("error reading the body of the ip request response: %s\n", err.Error())
 		c.exit()
 	}
 
 	current_ip := net.ParseIP(string(ip_bytes))
 
 	if current_ip == nil {
-		c.logger.Errorf("current IP address could not be parsed from '%s'", string(ip_bytes))
+		c.logger.Errorf("current IP address could not be parsed from '%s'\n", string(ip_bytes))
 		c.exit()
 	}
 
 	zones, err := c.api.ListZones(ctx, c.zone_name)
 
 	if err != nil {
-		c.logger.Errorf("could not list zones: %s", err.Error())
+		c.logger.Errorf("could not list zones: %s\n", err.Error())
 		c.exit()
 	}
 
 	if len(zones) < 1 {
-		c.logger.Errorf("no zones found for '%s'", c.zone_name)
+		c.logger.Errorf("no zones found for '%s'\n", c.zone_name)
 		c.exit()
 	}
 
@@ -125,12 +125,12 @@ func (c *CloudflareDDNSUpdaterApplication) update(ctx context.Context) {
 	})
 
 	if err != nil {
-		c.logger.Errorf("could not list records for '%s': %s", c.record_name, err.Error())
+		c.logger.Errorf("could not list records for '%s': %s\n", c.record_name, err.Error())
 		c.exit()
 	}
 
 	if len(records) < 1 {
-		c.logger.Errorf("no A records found for '%s'", c.record_name)
+		c.logger.Errorf("no A records found for '%s'\n", c.record_name)
 		c.exit()
 	}
 
@@ -141,18 +141,18 @@ func (c *CloudflareDDNSUpdaterApplication) update(ctx context.Context) {
 		})
 
 		if err != nil {
-			c.logger.Errorf("could not update record '%s' in zone '%s': %s", c.record_name, c.zone_name, err)
+			c.logger.Errorf("could not update record '%s' in zone '%s': %s\n", c.record_name, c.zone_name, err)
 			c.exit()
 		}
 	}
-	c.logger.Infof("CLOUDFLARE DDNS update finished " + strings.Repeat("-", 18))
+	c.logger.Infof("CLOUDFLARE DDNS update finished " + strings.Repeat("-", 18) + "\n")
 }
 
 func (c *CloudflareDDNSUpdaterApplication) run() {
+	ctx, cancel := context.WithCancel(context.Background())
+	c.context = ctx
+	c.cancel = cancel
 	for {
-		ctx, cancel := context.WithCancel(context.Background())
-		c.context = ctx
-		c.cancel = cancel
 		go c.update(c.context)
 		time.Sleep(5 * time.Minute)
 	}
